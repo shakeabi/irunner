a630b20a4f31e00eb5389142b7eda168135f6f43
Abishake <abishake.110899@gmail.com>
2018-06-04 22:00:07 +0530

    final

--- js/game.js
+++ js/game.js
@@ -2,6 +2,9 @@
 .Started
 .init setup
 .controlled wall freq using set timeout
+.added enemies
+.hackermode
+.prevented moving back wrt bg to prevent suiciding
 */
 
 
@@ -12,11 +15,14 @@ var ctx = canvas.getContext("2d");
 var cwidth = canvas.width;
 var cheight = canvas.height;
 
-var bgSpeed = 2;
-var wallSpeed = 2;
+var bgSpeed = 3;
+var wallSpeed = 3;
 var count = 0;
 var playerWidth = 50;
 var playerHeight = 50;	
+var gameStarted = false;
+var pause = false;
+var level = 0;
 
 
 const bg = new Image();
@@ -24,50 +30,87 @@ const wallimg = new Image();
 const spikey = new Image();
 const player = new Image();
 const spawn = new Image();
+const health = new Image();
+const introimg = new Image();
 
 bg.src = 'assets/img/bgtilen.jpg';
 wallimg.src = 'assets/img/walltile.jpg';
 spikey.src = 'assets/img/spikey.png';
 player.src = 'assets/img/player.png';
 spawn.src = 'assets/img/enemy.png';
+health.src = 'assets/img/heart.png';
+introimg.src = 'assets/img/introimg.jpg';
+
+const introaud = new Audio();
+const bgaud = new Audio();
+const gameoveraud = new Audio();
+const coin = new Audio();
+
+introaud.src = 'assets/audio/intromusic.mp3';
+bgaud.src = 'assets/audio/bgmusic.mp3';
+gameoveraud.src = 'assets/audio/gameover.mp3';
+coin.src = 'assets/audio/coin.mp3';
+
+introaud.loop = true;
+bgaud.loop = true;
 
 const env = new environment(canvas, bg, ctx);
 const walls = [];
-// const enemies = [];
+const enemies = [];
 var curWalls = [];
-const deep = new hero(0, 0, player, ctx);
+//var enemybullets = [];
+var upgradeList = [];
+const deep = new hero(50, 50, player, ctx);
 var mouse = {mouseX:50,mouseY:50};
 
-setInterval(function(){
-	var wallSet = generateWall();
-	walls.push(wallSet.top, wallSet.bottom);
-	// var enemy = generateEnemy();
-	// enemies.push(enemy);
-	
+// setInterval(function(){
+// 	var wallSet = generateWall();
+// 	walls.push(wallSet.top, wallSet.bottom);
+// 	if(walls.length>=4){
+//         var enemyPlayer = generateEnemy();
+//         enemies.push(enemyPlayer);
+    
+//     }
 
-},5000);
+    	
+
+// },5000);
 
 ///////////////////////////////////////////////
 //real one
 
-// var genwall = function(){
-// 	var wallSet = generateWall();
-// 	walls.push(wallSet.top, wallSet.bottom);
-// 	++count;
-// 	console.log(count);
-// 	if(count%2 == 0 && wallSpeed<20){
+var genwall = function(){
+	var wallSet = generateWall();
+	walls.push(wallSet.top, wallSet.bottom);
+	++count;
+	console.log(count);
+	if(wallSpeed<2){
+        wallSpeed = 2;
+        bgSpeed = 2;
+    }
+    if(count%2 == 0 && wallSpeed<20){
+	    level++;
+		wallSpeed++;;
+		bgSpeed++;
 	
-// 		wallSpeed++;;
-// 		bgSpeed++;
-	
-// 	}
-// 		console.log(wallSpeed);
+	}
+    if(walls.length>=4){
+        var enemyPlayer = generateEnemy();
+        enemies.push(enemyPlayer);
+    
+    }
+		console.log(wallSpeed);
 
-// 	setTimeout(genwall, Math.floor(20000/wallSpeed));
+    if(count%2==0){
+        var upgradetemp = generateUpgrade();
+        upgradeList.push(upgradetemp);
+    }	
+
+    setTimeout(genwall, Math.floor(20000/wallSpeed));
 
 
-// }
-// setTimeout(genwall, Math.floor(10000/wallSpeed));
+}
+
 
 /////////////////////////////////////////////////////
 
@@ -78,54 +121,211 @@ canvas.onmousemove = function(mouseTemp){
 	mouse.mouseY = mouseTemp.layerY;
 
 
-	if(mouse.mouseX<playerWidth/2)mouse.mouseX=playerWidth/2;
+	if(mouse.mouseX<40)mouse.mouseX=playerWidth/2+40;
 	if(mouse.mouseX>cwidth-playerWidth/2)mouse.mouseX=cwidth-playerWidth/2;
 	if(mouse.mouseY<playerHeight/2)mouse.mouseY=playerHeight/2;
 	if(mouse.mouseY>cheight-playerHeight/2)mouse.mouseY=cheight-playerHeight/2;
 }
 
+canvas.onmousedown = function(){
+    deep.shieldActive = true;
+}
+
+canvas.onmouseup = function(){
+    deep.shieldActive = false;
+}
+
 window.onload = function(){
 
+    intro();
 
-    gameLoop();
+    document.addEventListener('keydown',function(e){
+        if(gameStarted){
+            if(e.keyCode == 80){
+                if(pause == false){
+                    pause = true;
+                }
+                else {
+                    pause = false;
+                    gameLoop();
+                }
+            }
+            if(e.keyCode == 82){
+                window.location.reload();
+            }
+        }
+        else{
+            if(e.keyCode == 13){
+                gameStarted = true;
+                setTimeout(genwall, Math.floor(10000/wallSpeed));
+                gameLoop();
+            }
+            if(e.keyCode == 82){
+                window.location.reload();
+            }
+        }
 
-    function gameLoop(){
+    });
+    
 
-        ctx.clearRect(0, 0, cwidth, cheight);
+}
+
+
+function gameLoop(){
+    stopAudio(introaud);
+    bgaud.play();
+
+    ctx.clearRect(0, 0, cwidth, cheight);
+
+    bgEntitiesUpdate();
+    inGameEntitiesUpdate();
+    overlayEntitiesUpdate();
+    manageTrash();
+
+    if(pause == true){
+        pauseScreen();
+        return;
+    }
+
+
+    if(deep.hp>0)
+       window.requestAnimationFrame(gameLoop);
+    else
+       gameOver();
+
+}
+
+function bgEntitiesUpdate(){
         env.update(bgSpeed);
         env.render();
         walls.forEach(function(wall){
-        	wall.update(wallSpeed);
-	        wall.render();
+            wall.update(wallSpeed);
+            wall.render();
         });
        
         walls.forEach(function(wall){
-        	console.log(mouse);
-        	mouse = wall.entityCollision(deep, mouse);
+            mouse = wall.entityCollision(deep, mouse);
 
         });
 
-        deep.update(mouse);
+}
+
+function inGameEntitiesUpdate(){
+        enemies.forEach(function(enemytemp){
+            enemytemp.update();
+            enemytemp.render();
+            if(deep.shieldActive == false){
+                var isColliding = testCollisionEntity(deep,enemytemp);
+                    if(isColliding){
+                        deep.hp-=2;
+                 
+                    }
+            }
+
+        });
+
+        for(var key in upgradeList){
+            upgradeList[key].update();
+            upgradeList[key].render();
+
+            var isColliding = testCollisionEntity(deep,upgradeList[key]);
+               if(isColliding){
+                   switch(upgradeList[key].category){
+                    case 0: deep.hp += 30;break;
+                    case 1: deep.shield += 2000;break;
+                    case 2: bgSpeed = Math.round(bgSpeed/2);
+                            wallSpeed = Math.round(wallSpeed/2);
+                            break;
+                   }
+
+                   upgradeList.shift();
+                   coin.play();
+                 
+               }
+        }
+
+    
+        deep.update(mouse, bgSpeed);
         deep.render();
+
+}
+
+function overlayEntitiesUpdate(){
 
         ctx.drawImage(spikey, 0, 0, 83, 1120, 0, 0, 40, cheight);
         
+        ctx.save();
+        ctx.globalAlpha = 0.4;
+        ctx.fillStyle = "#00000f";
+        ctx.fillRect(0, 0, cwidth, 50);
+        ctx.restore();
+        ctx.drawImage(health, 70, 5, 40, 40);
+            var my_grad=ctx.createLinearGradient(cwidth*.1, 0, cwidth*.1+150, 0);
+            my_grad.addColorStop(0,"firebrick");
+            my_grad.addColorStop(0.5,"gold");
+            my_grad.addColorStop(1,"limegreen");
+            ctx.fillStyle = "black"
+            ctx.fillRect(cwidth*.1, 10, 150, 30);
+            ctx.fillStyle=my_grad;
+            ctx.strokeStyle = "black";
+            if(deep.hp>=0){
+                ctx.fillRect(cwidth*.1, 10, deep.hp*3/2, 30);
+            }
+            ctx.strokeRect(cwidth*.1, 10, 150, 30);
+
         ctx.fillStyle = '#ffffff';
         ctx.font = '20px Georgia';
-        ctx.fillText('Score:'+deep.score, cwidth-100, 25);
+        ctx.fillText('Shield: '+ Math.round(Math.ceil(deep.shield)/10000*100) + "%", 300, 30);
+        ctx.fillText('Level: '+ level, cwidth-200, 30);
+        ctx.fillText('Score: '+ deep.score, cwidth-100, 30);
         
+}
+
+function manageTrash(){
+
         if(walls.length!=0 && walls[0].xpos <= -76){//76 is wall width and this makes sure only walls on screen exist
-        	walls.shift();
-        	walls.shift();
-        	deep.score++;
+            walls.shift();
+            walls.shift();
+            deep.score++;
         }
-       	
 
-        window.requestAnimationFrame(gameLoop);
-    }
+        if(enemies.length!=0 && enemies[0].x <= -50){//25 is ghost width and this makes sure only ghosts on screen exist
+            enemies.shift();
+            
+        }
 
-    
+        console.log("upgaea ",upgradeList.length);
+        if(upgradeList.length!=0 && upgradeList[0].x <= -50){//25 is ghost width and this makes sure only ghosts on screen exist
+            upgradeList.shift();
+            
+        }
+        
+}
 
+function intro(){
+    introaud.play();
+    ctx.save();
+    ctx.drawImage(introimg, 0, 0, cwidth, cheight);
+    ctx.fillStyle= "white";
+    ctx.font="80px Georgia";
+    ctx.fillText("Press Enter to start!", cwidth/2-300, 200);
+    ctx.font="40px Georgia"
+    ctx.fillText("Press p for pause or rules.", cwidth/2-230, 400);
+    ctx.restore();
+}
+
+function gameOver(){
+    stopAudio(bgaud);
+    gameoveraud.play();
+    ctx.save();
+    ctx.fillText('Game Over', 100, 100);
+    ctx.restore();
+}
+
+function pauseScreen(){
+    ctx.save();
+    ctx.fillText('pauseScreen', 100, 100);
+    ctx.restore();
 }
 
 function generateWall(){
@@ -137,22 +337,45 @@ function generateWall(){
 	return returnData;
 }
 
-// function generateEnemy(){
-// 	var randx = Math.round(Math.random()*walls[walls.length-1].ypos-20 + walls[walls.length-2].ypos + walls[walls.length-2].thickness + 20);
-// 	var randy = Math.round(Math.random()*(cheight-25)+25);  /////////////////////////////////////////////////////////////////////////////////////////////////////////////change it accordingly
-// 	var returnData = new enemy(randx, randy, spawn, canvas, ctx);
-// }
+function generateEnemy(){
+	var randx = Math.round(Math.random()*(cwidth-100)+600); //+ (walls[walls.length-4].xpos + walls[walls.length-4].thickness + 30));
+	var randy = Math.round(Math.random()*(cheight-50)+50);  
+
+	var returnData = new enemy(randx, randy, spawn, wallSpeed, canvas, ctx);
+    return returnData;
+    
+}
+
+function generateUpgrade(){
+    var randx = Math.round(Math.random()*(cwidth-100)+600); 
+    var randy = Math.round(Math.random()*(cheight-100)+100);  
+    var prob = Math.random()*100;
+    //probability of upgrade spawning.
+    if(prob<=10)
+        var returnData = new upgrade(randx, randy, bgSpeed, 2, canvas, ctx);
+    else if(prob>10 && prob<=50) 
+            var returnData = new upgrade(randx, randy, bgSpeed, 0, canvas, ctx);
+         else var returnData = new upgrade(randx, randy, bgSpeed, 1, canvas, ctx);
+    
+    return returnData;
+    
+}
+
+function getDistanceBetweenEntity(entity1,entity2){  //return distance (number)
+        var vx = entity1.x - entity2.x;
+        var vy = entity1.y - entity2.y;
+        return Math.sqrt(vx*vx+vy*vy);
+}
+
+function testCollisionEntity(entity1,entity2){       //return if colliding (true/false)
+        var distance = getDistanceBetweenEntity(entity1,entity2);
+        return distance < 40;
+}
+
+function stopAudio(audio) {  
+    audio.pause();
+    audio.currentTime = 0;
+}
 
 
 
-
-//define all variables
-
-
-
-//images
-
-
-
-canvas.setAttribute('tabindex','0');
-canvas.focus();
