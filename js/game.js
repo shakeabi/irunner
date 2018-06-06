bf2fba231e4de2f66c472213faf7fb3806601309
Abishake <abishake.110899@gmail.com>
2018-06-06 19:31:41 +0530

    final cleanup

--- js/game.js
+++ js/game.js
@@ -5,6 +5,9 @@
 .added enemies
 .hackermode
 .prevented moving back wrt bg to prevent suiciding
+.added bullets
+.added sprites
+.added sounds
 */
 
 
@@ -31,6 +34,8 @@ const spikey = new Image();
 const player = new Image();
 const spawn = new Image();
 const health = new Image();
+const shieldimg = new Image();
+const timeimg = new Image();
 const introimg = new Image();
 
 bg.src = 'assets/img/bgtilen.jpg';
@@ -40,16 +45,20 @@ player.src = 'assets/img/player.png';
 spawn.src = 'assets/img/enemy.png';
 health.src = 'assets/img/heart.png';
 introimg.src = 'assets/img/introimg.jpg';
+shieldimg.src = 'assets/img/shield.png';
+timeimg.src = 'assets/img/time.png';
 
 const introaud = new Audio();
 const bgaud = new Audio();
 const gameoveraud = new Audio();
 const coin = new Audio();
+const hurt = new Audio();
 
 introaud.src = 'assets/audio/intromusic.mp3';
 bgaud.src = 'assets/audio/bgmusic.mp3';
 gameoveraud.src = 'assets/audio/gameover.mp3';
 coin.src = 'assets/audio/coin.mp3';
+hurt.src = 'assets/audio/hurt.mp3';
 
 introaud.loop = true;
 bgaud.loop = true;
@@ -58,7 +67,7 @@ const env = new environment(canvas, bg, ctx);
 const walls = [];
 const enemies = [];
 var curWalls = [];
-//var enemybullets = [];
+var enemyBullets = [];
 var upgradeList = [];
 const deep = new hero(50, 50, player, ctx);
 var mouse = {mouseX:50,mouseY:50};
@@ -79,16 +88,16 @@ var mouse = {mouseX:50,mouseY:50};
 ///////////////////////////////////////////////
 //real one
 
-var genwall = function(){
+var genwall = function(){//generates walls enemies bullets and upgrades
 	var wallSet = generateWall();
 	walls.push(wallSet.top, wallSet.bottom);
 	++count;
-	console.log(count);
+
 	if(wallSpeed<2){
         wallSpeed = 2;
         bgSpeed = 2;
     }
-    if(count%2 == 0 && wallSpeed<20){
+    if(count%3 == 0 && wallSpeed<20){
 	    level++;
 		wallSpeed++;;
 		bgSpeed++;
@@ -97,11 +106,15 @@ var genwall = function(){
     if(walls.length>=4){
         var enemyPlayer = generateEnemy();
         enemies.push(enemyPlayer);
-    
-    }
-		console.log(wallSpeed);
+        if(enemies.length!=0){
 
-    if(count%2==0){
+            var bullettemp = generateBullet();
+            enemyBullets.push(bullettemp);
+       
+        }
+    }
+
+    if(count%4==0){
         var upgradetemp = generateUpgrade();
         upgradeList.push(upgradetemp);
     }	
@@ -217,9 +230,25 @@ function inGameEntitiesUpdate(){
             if(deep.shieldActive == false){
                 var isColliding = testCollisionEntity(deep,enemytemp);
                     if(isColliding){
-                        deep.hp-=2;
-                 
+                        deep.hp-=0.2;
+                 		hurt.play();
                     }
+            }
+
+        });
+
+
+        enemyBullets.forEach(function(bulletkey){
+            bulletkey.update();
+            bulletkey.render();
+            if(deep.shieldActive == false){
+                var isColliding = testCollisionEntity(deep,bulletkey);
+                    if(isColliding){
+                        deep.hp-=5;
+                        hurt.play();
+                 		enemyBullets.shift();
+                    }
+
             }
 
         });
@@ -294,7 +323,7 @@ function manageTrash(){
             
         }
 
-        console.log("upgaea ",upgradeList.length);
+
         if(upgradeList.length!=0 && upgradeList[0].x <= -50){//25 is ghost width and this makes sure only ghosts on screen exist
             upgradeList.shift();
             
@@ -306,11 +335,34 @@ function intro(){
     introaud.play();
     ctx.save();
     ctx.drawImage(introimg, 0, 0, cwidth, cheight);
-    ctx.fillStyle= "white";
-    ctx.font="80px Georgia";
-    ctx.fillText("Press Enter to start!", cwidth/2-300, 200);
-    ctx.font="40px Georgia"
-    ctx.fillText("Press p for pause or rules.", cwidth/2-230, 400);
+    
+    ctx.fillStyle = "white";
+    ctx.font = "50px Georgia";
+    ctx.fillText("IRunner",cwidth/2-90,120);
+    ctx.fillStyle = "red";
+    ctx.font = "25px Trebuchet MS";
+    ctx.fillText("The dungeon is haunted by ghosts.",cwidth/2-150,190);
+    ctx.fillText("Touching them will result in loss of health.",cwidth/2-200,225);
+    ctx.fillText("They throw vicious spells that can damage your health very badly.",cwidth/2-320,260);
+    ctx.fillText("WARNING: Their magic spells can pass through walls!",cwidth/2-250,295);
+    
+    ctx.fillStyle = "white";
+    ctx.fillText("Collect the upgrades.",80,380);
+    ctx.fillText("Left click to activate shield.",80,415);
+    ctx.fillText("Shield protects you from ghosts and their spells.",80,445);
+    ctx.fillText("Use shield carefully, as it is limited!",80,480);
+    ctx.fillText("Crushing against spike will kill you!",80,515);
+    ctx.fillText("Your score is calculated based on no. of walls you cross!",80,550);
+    ctx.fillText("Upgrades",900,400);
+    ctx.fillStyle = "yellow";
+    ctx.fillText("Increases Health",900,445);
+    ctx.fillText("Increases shield",900,480);
+    ctx.fillText("Slows down time",900,515);
+    ctx.drawImage(health, 1100, 420, 30, 30);
+    ctx.drawImage(shieldimg, 1100, 455, 30, 30);
+    ctx.drawImage(timeimg, 1100, 490, 30, 30);
+
+ 
     ctx.restore();
 }
 
@@ -318,13 +370,28 @@ function gameOver(){
     stopAudio(bgaud);
     gameoveraud.play();
     ctx.save();
-    ctx.fillText('Game Over', 100, 100);
+    ctx.globalAlpha = 0.4;
+    ctx.fillStyle = "black";
+    ctx.fillRect(100,130,1000,320);
+    ctx.globalAlpha = 1;
+    ctx.fillStyle= "white";
+    ctx.font="40px Georgia";
+    ctx.fillText("Game Over!", cwidth/2-115, 200);
+    ctx.fillText("Press 'r' to Restart!" , cwidth/2-175, 300);
+    ctx.fillText("Score: " + deep.score, cwidth/2-90, 400);
     ctx.restore();
 }
 
 function pauseScreen(){
     ctx.save();
-    ctx.fillText('pauseScreen', 100, 100);
+    ctx.globalAlpha = 0.4;
+    ctx.fillStyle = "black";
+    ctx.fillRect(100,150,1000,320);
+    ctx.globalAlpha = 1;
+    ctx.fillStyle= "white";
+    ctx.font="40px Georgia";
+    ctx.fillText("Game Paused!", cwidth/2-135, 240);
+    ctx.fillText("Press 'p' to resume!!", cwidth/2-180, 400);
     ctx.restore();
 }
 
@@ -344,6 +411,19 @@ function generateEnemy(){
 	var returnData = new enemy(randx, randy, spawn, wallSpeed, canvas, ctx);
     return returnData;
     
+}
+
+function generateBullet(){
+    var randx = enemies[enemies.length-1].x;
+    var randy = enemies[enemies.length-1].y;
+
+    var tan = ((deep.y-enemies[enemies.length-1].y)/(deep.x-enemies[enemies.length-1].x));
+    // tan += Math.PI;
+    var angle = Math.atan(tan);
+    var spdX = Math.cos(angle)*10;
+    var spdY = Math.sin(angle)*10;
+    var returnData = new bullet(randx, randy, spdX, spdY, ctx);
+    return returnData;
 }
 
 function generateUpgrade(){
